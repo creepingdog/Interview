58465377e6f83d7f01fcf96f48622459b0b696a1
ahu <Yeoz0320>
2019-06-15 15:46:04 -0400

    late_binding

---
+++ exe/late_binding.py
@@ -0,0 +1,29 @@
+# Actually the difference shown below involves several concepts:
+# 1. Closure
+# 2. List/Tuple comprehensive
+# 3. Late binding
+
+
+def create_multipliers_list():
+    return [lambda x: i * x for i in range(5)]
+#
+
+
+def create_multipliers_tuple():
+    return (lambda x: i * x for i in range(5))
+#
+
+
+if __name__ == '__main__':
+    print('===== create_multipliers_list =====')
+    for multiplier in create_multipliers_list():
+        print(multiplier(2))
+    #
+
+    print()
+
+    print('===== create_multipliers_tuple =====')
+    for multiplier in create_multipliers_tuple():
+        print(multiplier(2))
+    #
+#
