a912b88c1fdc96f4ca4e5fc462c141c3229b84be
ahu <Yeoz0320>
2019-08-19 10:12:22 -0700

    hex representation and reordering of the prints

--- python3_intermediate/0001.new_vs_init.py
+++ python3_intermediate/0001.new_vs_init.py
@@ -1,7 +1,7 @@
 class A(object):
     def __new__(cls, *args, **kwargs):
-        print('[A::__new__] This is the ID of cls {}: {}'.format(cls, id(cls)))
-        clz = object.__new__(cls)
+        print('[A::__new__] This is the ID of cls {}: {}({})'.format(cls, id(cls), hex(id(cls))))
+        clz = super().__new__(cls)
         print('[A::__new__] This is the return of __new__ method: {}'.format(clz))
         return clz
     #
@@ -13,6 +13,6 @@ class A(object):
 
 
 if __name__ == '__main__':
+    print('This is the ID of Class A: {}({})'.format(id(A), hex(id(A))))
     a = A()
-    print('This is the ID of Class A: {}'.format(id(A)))
-    print('This is the ID of an instance of Class A: {}'.format(id(a)))
+    print('This is the ID of an instance of Class A: {}({})'.format(id(a), hex(id(a))))
